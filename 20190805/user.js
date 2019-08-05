f0572406e9d8d90889df2d5d9cf9f94e61c7dcc1
452366we <2754464094@qq.com>
2019-08-05 19:58:51 +0800

    20190805

---
+++ 20190805/user.js
@@ -0,0 +1,7 @@
+//将来在企业项目中，都是讲操作一种对象的所有函数，封装在一个大的对象中
+var user={
+    signin:()=>console.log("登录"),
+    signout:()=>console.log("注销"),
+    signup:()=>console.log("注册"),
+    changepwd:()=>console.log("修改密码"),
+}
